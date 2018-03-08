a09cc62cfea7a71e897587f66c5e97b94a765db6
Mike Jay <mjay@ripcord.tech>
2018-03-08 15:18:26 -0500

    more depth to index page and added face pic

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <link href="css/site.css"rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico"/>
-<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
 
 </head>
 <body>
@@ -39,17 +39,39 @@
             ?>
     </ul>
     </nav>
+    <div class="page">
     <div class="container-fluid"align="center">
-          <div class="row">
-              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText" ><h1 class="shakeyText"><b>Hi, I'm Mike.</b></h1><br><h1 class="shakeyText"><b>I am an application developer.</b></h1></div>
-          </div>
-      </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 " >
+                    <div class="circleBase mePic"></div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid"align="center">
+            <div class="row">
+                <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText" ><h1><b>Hi, I'm Mike Jay.</b></h1><br><h4><b>I am an application developer from Toronto Ontario.</b></h4><br>
+                <h4>Writing code is my passion, and I enjoy utilizing my <span style="color:green">skills</span> for working in the full web stack to bring ideas to reality.</h4>
+                <h4 style="margin-top:1em;margin-bottom:1em;">Are you looking to build your own application or website? Or are you looking to hire a developer?</h4>
+                </div>
+            </div>
+        </div>
+    
       <div class="container-fluid"align="center">
           <div class="row">
-              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText" ><a class="scroll"target="#firstCarousel"><i id="downArrow" style="font-size:4em" class="fas fa-angle-down"></i></a></div>
+              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12" ><button type="button"class="btn btn-lg btn-outline-dark my-2 my-lg-6"id="contactbutton">Contact</button>
+            </div>
           </div>
       </div>
+
+        <div class="container-fluid"align="center">
+          <div class="row">
+              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 " ><i class="arrow down"id="downArrow"></i></div>
+          </div>
+      </div>
+    </div>
+    </div>
       
+<div class="page">
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -57,10 +79,17 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
+  <div class="container-fluid"align="center">
+          <div class="row" id="carouselTarget">
+              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"><i class="arrow up"id="upArrow"></i></div>
+          </div>
+      </div>
   <div class="carousel-inner">
   <div class="carousel-inner">
-    <div class="carousel-item active c" id="firstCarousel">
+ 
+    <div class="carousel-item active " id="firstCarousel">
     <div class="container-fluid">
+        
           <div class="row">
               <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center" ><h1>Are you looking for a full stack application developer</h1><br>
               <h1>to build your application or website from design to deployment?</h1>
@@ -69,38 +98,35 @@
           </div>
       </div>
     </div>
-    <div class="carousel-item c">
+    <div class="carousel-item ">
     <div class="container-fluid">
           <div class="row">
-              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center"><h1>Are you looking for a full stack application developer</h1><br>
-              <h1>to build your application or website from design to deployment?</h1>
+              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center"><h1>I am a team player and my skills are always growing</h1><br>
+              <h1>Active user of Git source control for feature develoment and staying connected with Slack</h1>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="carousel-item ">
+    <div class="container-fluid">
+          <div class="row">
+              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center"><h1>Enjoy building database driven apps with</h1><br>
+              <h1>MSSQL and MYSQL relational databases on the back-end.</h1>
 
             </div>
           </div>
       </div>
     </div>
-    <div class="carousel-item c">
+    <div class="carousel-item ">
     <div class="container-fluid">
           <div class="row">
-              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center"><h1>Are you looking for a full stack application developer</h1><br>
-              <h1>to build your application or website from design to deployment?</h1>
+              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center"><h1>Register on this site for a sample</h1><br>
+              <h1>database driven profile application</h1>
 
             </div>
           </div>
       </div>
     </div>
-    <div class="carousel-item c">
-    <div class="container-fluid">
-          <div class="row">
-              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText"align="center"><h1>Are you looking for a full stack application developer</h1><br>
-              <h1>to build your application or website from design to deployment?</h1>
-
-            </div>
-          </div>
-      </div>
-    </div>
-  </div>
-   
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -111,12 +137,17 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-<div class="container-fluid"align="center">
-          <div class="row">
-              <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 carText" ><h2>This </h2></div>
-          </div>
-      </div>
-
+    <div class="container-fluid"align="center">
+            <div class="row">
+                <div class="col-lg-12 col-md-6 col-sm-4 col-xs-12 " ><i class="arrow down"id="downArrow2"></i>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+<div class="page">
+    
+</div>
 <script src="js/jquery-3.1.1.min.js"type="text/javascript"></script>
 <script src="js/jquery.validate.min.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js"type="text/javascript"></script>
--- js/global.js
+++ js/global.js
@@ -1,11 +1,19 @@
 
 $(document).ready(function(){
-   /*  $('#downArrow').click(function(){
-        $('html, body'),animate({
-            scrollTop: $('#firstCarousel').offset().top
-        },2000)
-    }) */
-   $
-
-
+    $('#downArrow').click(function(){
+        $('html, body').animate({
+            scrollTop: $('#carouselTarget').offset().top
+        },500)
+    })
+    $('#upArrow').click(function(){
+        $('html, body').animate({
+            scrollTop: $('.nervbar').offset().top
+        },500)
+    })
+    $('#contactbutton').click(function(){
+        $(location).attr('href','contact.php')
+    })
+    $('.carousel').carousel({
+        interval: 3500
+      })
 })
