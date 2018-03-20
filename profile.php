a12a636f7d20ff588602467b8a9e6e3431b3492d
unlikemikeshmay <michael.a.t.jay@gmail.com>
2018-03-20 11:32:36 -0400

    finished about and info

--- profile.php
+++ profile.php
@@ -183,7 +183,7 @@ if(isset($_SESSION['EMAIL']))
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     echo "<meta http-equiv='refresh'content='0;url='profile.php'>";
   
-   var_dump($_FILES['file']['tmp_name']);
+ /*   var_dump($_FILES['file']['tmp_name']); */
     if($_FILES!=null){
         echo 'dicks';
         $imageData = file_get_contents($_FILES['file']['tmp_name']);
@@ -229,42 +229,23 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
            
         </div>
            
-    </div>
-    
+    </div> 
 </div>
-          
-          
-
 </div>
 <div class="container">
     <div class="row my-2">
-        <div class="col-lg-8 order-lg-2">
+        <div class="col-lg-12 order-lg-2">
             <ul class="nav nav-tabs">
                 <li class="nav-item">
                     <a href="" data-target="#profile" data-toggle="tab" class="nav-link active">Profile</a>
                 </li>
-              <script>
-              $(document).ready(function(){
-                var image = document.createElement('img');
-    
-    image.src="data:image/gif;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==";
-        
-    image.width=100;
-    image.height=100;
-    image.alt="here should be some image";
-        
-    document.body.appendChild(image);
-              })
-              </script>
             </ul>
             <div class="tab-content py-4">
                 <div class="tab-pane active" id="profile">
                     <h5 class="mb-3">Welcome to your profile <?php echo($_SESSION['FirstName'] . ' ' . $_SESSION['LastName'])?></h5>
                     <div class="row">
-                        <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
-                           <div>
-                                 <a class="btn btn-lg btn-outline-dark my-2 my-lg-6" href="#collapseAbout1"data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample">About </a>
-                            </div>
+                        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12"align="left">
+                           <div><a class="btn btn-lg btn-outline-dark ml-3 my-2 my-lg-6" href="#collapseAbout1"data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample">About </a></div>
                                <div class="collapse" id="collapseAbout1">
                                     <div class="card card-body">
                                     <?php   if (!empty($_SESSION['ABOUT'])){
@@ -272,8 +253,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                                         echo('
                                         <form action="profile.php"method="POST" enctype="multipart/form-data" id="AboutChangeForm" onsubmit="reload()">
                                         <div class="form-group"> 
-                                        <label class="col-lg-4 ">'.$_SESSION['ABOUT'].'</label>                                      
-                                        <input type="text" class="form-control" name="about" id="about" placeholder="Write a little about you.">
+                                        <p>'.$_SESSION['ABOUT'].'</p>                                 
+                                        <input type="text" class="form-control" name="about1" id="about1" placeholder="Write a little about you.">
                                         <div>
                                         <input type="submit" name="submitAbout" class="form-control btn btn-sm btn-outline-dark" id="submitAbout" value="submit">
                                         </div>
@@ -283,43 +264,83 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                                         {echo('
                                             <form action="profile.php"method="POST" enctype="multipart/form-data" id="AboutChangeForm" onsubmit="reload()">
                                             <div class="form-group">
-                                            <label class="col-sm-2 col-form-label">Information about you will go here!</label>    
-                                            <input type="text" class="form-control" name="about" id="about" placeholder="Write a little about you.">
+                                            <p>Information about you goes here!</p>
+                                            <input type="text" class="form-control" name="about1" id="about1" placeholder="Write a little about you.">
                                             <div>
                                             <input type="submit" name="submitAbout" class="form-control btn btn-sm btn-outline-dark" id="submitAbout" value="submit">
                                             </div>
                                             </div></form>');
-                                        } 
+                                        }
+                                        if($_SERVER['REQUEST_METHOD'] == "POST"){
+                                            echo "<meta http-equiv='refresh'content='0;url='profile.php'>";    
+                                            if(isset($_POST['about1'])){
+                                                
+                                                $aboutData = $_POST['about1'];
+                                                $aboutDataForDb = mysqli_real_escape_string($db,$aboutData);
+                                        
+                                                $email =  $_SESSION['EMAIL'];
+                                                $stmt = $db->prepare("UPDATE accounts SET about = '$aboutDataForDb' where email ='$email'");
+                                                if($stmt->execute())
+                                                {
+                                                    echo '
+                                                    <div class="container">
+                                                    <div class="row">
+                                                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                                        <h4> great success with about</h4>' . $_SESSION['EMAIL'] . ' 
+                                                    </div>    
+                                                    </div>
+                                                    </div>';                
+                                                }   
+                                                }
+                                            else {
+                                                echo '
+                                                    <div class="container">
+                                                    <div class="row">
+                                                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                                                        <h4> great failure with about</h4>' . $_SESSION['EMAIL'] . ' 
+                                                    </div>    
+                                                    </div>
+                                                    </div>';
+                                               }                                                            
+                                        }
                                         ?>
-                                        <!--   Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. -->
+                                       
                                     </div>
                                 </div>
                             </div>
-                            <div>
-                                <a class="btn btn-lg btn-outline-dark my-2 my-lg-6" href="#collapseAbout2"data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample">Info</a>
-                            </div>  
+                            </div>
+                            <hr>
+                            <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12"align="left">
+                           <div><a class="btn btn-lg btn-outline-dark my-2 my-lg-6" href="#collapseAbout2"data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample">Info </a></div>
                                <div class="collapse" id="collapseAbout2">
                                     <div class="card card-body">
-                                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                    <?php   if (!empty($_SESSION['ABOUT'])){
+                                        
+                                        echo('<ul class="list-group">
+                                        <li class="list-group-item">Email: '.$_SESSION['EMAIL'].'</li>
+                                        <li class="list-group-item">First name: '.$_SESSION['FirstName'].' </li>
+                                        <li class="list-group-item">Last name: '.$_SESSION['LastName'].'</li>
+                                        <li class="list-group-item">About: '.$_SESSION['ABOUT'].'</li>
+                                        <li class="list-group-item">Registered Since: '.$_SESSION['REGIDATE'].'</li>
+                                      
+                                      </ul>');
+                                        }
+                                        else
+                                        {echo('<ul class="list-group">
+                                            <li class="list-group-item">Email:</li>
+                                            <li class="list-group-item">First name: </li>
+                                            <li class="list-group-item">Last name:</li>
+                                            <li class="list-group-item">About:</li>
+                                          
+                                          </ul>');
+                                        } 
+                                        ?>
+                                       
                                     </div>
                                 </div>
                             </div>
-                               
-                        
-                        <div class="col-md-6">
-                            <h6>Recent badges</h6>
-                            <a href="#" class="badge badge-dark badge-pill">html5</a>
-                  
-                            <a href="#" class="badge badge-dark badge-pill">angularjs</a>
-                            <a href="#" class="badge badge-dark badge-pill">css3</a>
-                            <a href="#" class="badge badge-dark badge-pill">jquery</a>
-                            <a href="#" class="badge badge-dark badge-pill">bootstrap</a>
-                            <a href="#" class="badge badge-dark badge-pill">responsive-design</a>
-                            <hr>
-                            <span class="badge badge-primary"><i class="fa fa-user"></i> 0 Followers</span>
-                            <span class="badge badge-success"><i class="fa fa-cog"></i> 0 Forks</span>
-                            <span class="badge badge-danger"><i class="fa fa-eye"></i>0 Views</span>
-                        </div>
+                            </div>
+                       <!--  -->
                         <div class="col-md-12">
                             <h5 class="mt-2"><span class="fa fa-clock-o ion-clock float-right"></span> Recent Activity</h5>
                             <table class="table table-sm table-hover table-striped">
@@ -334,102 +355,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                         </div>
                     </div>
                     <!--/row-->
-                </div>
-               
-               <!--  <div class="tab-pane" id="edit">
-                    <form role="form">
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">First name</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="text" value="Jane">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Last name</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="text" value="Bishop">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Email</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="email" value="email@gmail.com">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Company</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="text" value="">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Website</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="url" value="">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Address</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="text" value="" placeholder="Street">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label"></label>
-                            <div class="col-lg-6">
-                                <input class="form-control" type="text" value="" placeholder="City">
-                            </div>
-                            <div class="col-lg-3">
-                                <input class="form-control" type="text" value="" placeholder="State">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Time Zone</label>
-                            <div class="col-lg-9">
-                                <select id="user_time_zone" class="form-control" size="0">
-                                    <option value="Hawaii">(GMT-10:00) Hawaii</option>
-                                    <option value="Alaska">(GMT-09:00) Alaska</option>
-                                    <option value="Pacific Time (US &amp; Canada)">(GMT-08:00) Pacific Time (US &amp; Canada)</option>
-                                    <option value="Arizona">(GMT-07:00) Arizona</option>
-                                    <option value="Mountain Time (US &amp; Canada)">(GMT-07:00) Mountain Time (US &amp; Canada)</option>
-                                    <option value="Central Time (US &amp; Canada)" selected="selected">(GMT-06:00) Central Time (US &amp; Canada)</option>
-                                    <option value="Eastern Time (US &amp; Canada)">(GMT-05:00) Eastern Time (US &amp; Canada)</option>
-                                    <option value="Indiana (East)">(GMT-05:00) Indiana (East)</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Username</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="text" value="janeuser">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Password</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="password" value="11111122333">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label">Confirm password</label>
-                            <div class="col-lg-9">
-                                <input class="form-control" type="password" value="11111122333">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-lg-3 col-form-label form-control-label"></label>
-                            <div class="col-lg-9">
-                                <input type="reset" class="btn btn-secondary" value="Cancel">
-                                <input type="button" class="btn btn-primary" value="Save Changes">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-       
-    </div>
-</div> -->
+                </div>           
     <script src="js/jquery-3.1.1.min.js"type="text/javascript"></script>
 <script src="js/jquery.validate.min.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js"type="text/javascript"></script>
@@ -437,10 +363,3 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <script src="js/validate.js" type="text/javascript"></script>
 </body>
 </html>
-        
-     
-     
-
-<!-- Id guid?  FirstName varchar(15) 
-    Last name varchar(12) 
-   
